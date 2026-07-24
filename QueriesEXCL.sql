7e42e6630c596b6e10b8c39964b133b0f1c359dd
Achumile01 <somdakaachumile727@gmail.com>
2026-07-24 09:46:02 -0800

    Add SQL queries for user event analysis
    Added various SQL queries to count user events and analyze traffic sources.

--- QueriesEXCL.sql
+++ QueriesEXCL.sql
@@ -1,6 +1,47 @@
+USE ExcelPracticeDB;
+ SELECT*FROM dbo.user_events;
 
---creating a view for the most purchased product--
-CREATE VIEW vw_ConversionRateOfTrafficSources AS
+---Count number of User_id's---
+SELECT Count(Distinct user_id) AS TotalUsers From dbo.user_events;
+
+---Count number of event_id's---
+SELECT COUNT (event_id) AS TOTALEVENTS FROM dbo.user_events;
+
+---Count number of Page Views---
+SELECT COUNT(event_type) AS PAGEVIEWS FROM dbo.user_events
+WHERE event_type = 'page_view';
+
+---Count Number of add to carts---
+SELECT COUNT (event_type) AS [TOTAL ADD TO CARTS] FROM dbo.user_events
+WHERE event_type ='add_to_cart';
+
+----Count Number of Checkouts---
+SELECT COUNT (event_type) AS [TOTAL CHECK OUTS MADE] FROM dbo.user_events
+WHERE event_type='checkout_start';
+
+----Count Number of people who checked out and gave their payment info---
+SELECT COUNT (event_type) AS [TOTAL PAYMENT INFO] FROM dbo.user_events
+WHERE event_type='payment_info';
+
+----Count Number of people who Actually Purchased---
+SELECT COUNT (event_type) AS [TOTAL PURCHASES MADE] FROM dbo.user_events
+WHERE event_type='purchase';
+
+---LIST THE MOST PURCHASED PRODUCTS ACCORDING TO THEIR ID---
+SELECT product_id ,
+COUNT (*) AS [NUMBER OF PURCHASED PRODUCTS]
+FROM dbo.user_events
+GROUP BY product_id
+ORDER BY [NUMBER OF PURCHASED PRODUCTS] DESC;
+
+---LIST THE TRAFFIC_SOURCES,THE VIEWS AND PURCHASES MADE THROUGH THEM---
+SELECT traffic_source,
+COUNT(DISTINCT CASE WHEN event_type = 'Purchase' THEN user_id END) AS Purchases,
+COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN  user_id END) AS Views
+FROM dbo.user_events
+GROUP BY traffic_source;
+
+---CONVERSION RATE OF THE TRAFFIC SOURCES--
 SELECT traffic_source,
 COUNT(DISTINCT CASE WHEN event_type = 'Purchase' THEN user_id END) AS Purchases,
 COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN  user_id END) AS Views,
